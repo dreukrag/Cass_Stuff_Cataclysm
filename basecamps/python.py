c5c12b7d0c256d65ba1cd021a99f18197948597b
Filipe Groh <filipegroh@gmail.com>
2020-07-07 10:24:06 -0300

    setup

---
+++ basecamps/python.py
@@ -0,0 +1,34 @@
+import os, json
+
+# x = [i[2] for i in os.walk('.')]
+# y=[]
+# for t in x:
+#     for f in t:
+#         if f.endswith(".json"):
+#             y.append(f)
+# x = 
+# [os.path.join(r,file) for r,d,f in os.walk(".") for file in f]
+x = []
+for r,d,f in os.walk("."):
+    for file in f:
+        if file.endswith(".json"):
+            x.append(os.path.join(r,file))
+
+print(x)
+
+for file in x:
+    with open(file, "r+") as fr:
+        print(file)
+        data = json.load(fr)
+        for recipe in data:
+            if "time" in recipe:
+                print(recipe["time"])
+                recipe["time"] = "1 s"
+            elif "blueprint_needs" in recipe:
+                if "time" in recipe["blueprint_needs"]:
+                    print(recipe["blueprint_needs"]["time"])
+                    recipe["blueprint_needs"]["time"] = "1 s"
+        fr.seek(0)
+        print(data)
+        json.dump(data, fr, indent=4)
+    ##break
